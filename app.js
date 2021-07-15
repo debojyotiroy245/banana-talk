81549af66886d5f26d56360e01faff53cc9603ed
Debojyoti Roy <debojyotiroy245@gmail.com>
2021-07-15 23:18:58 +0530

    added text area and input

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hey")
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector('#txt-input')
+function clickHandler() {
+    console.log("clicked")
+    console.log("input",txtInput.value)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
